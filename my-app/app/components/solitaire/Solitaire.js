f9329f2506c06be5675628d9b10f61995c0f27b6
visegripped <jt@visegripped.com>
2024-04-22 07:03:31 -0700

    Aces can now be added to the building piles

--- my-app/app/components/solitaire/Solitaire.js
+++ my-app/app/components/solitaire/Solitaire.js
@@ -1,10 +1,10 @@
-'use client';
-import { useReducer, useState } from 'react';
-import { numTableaus, suits, cards } from '../../constants.mjs';
+"use client";
+import { useReducer, useState } from "react";
+import { numTableaus, suits, cards } from "../../constants.mjs";
 import { shuffleArray, buildDeck, deal } from "../../cardUtilities.mjs";
-import Tableau from '../tableau/Tableau';
-import Card from '../card/Card';
-import Pile from '../pile/Pile';
+import Tableau from "../tableau/Tableau";
+import Card from "../card/Card";
+import Pile from "../pile/Pile";
 
 //solitaire anatomy: https://www.britannica.com/topic/solitaire-card-game
 //reducer tutorial: https://blog.logrocket.com/react-usereducer-hook-ultimate-guide/
@@ -15,62 +15,132 @@ export default function Solitaire(props) {
   const fullDeck = buildDeck(suits, cards);
   const shuffledDeck = shuffleArray(fullDeck);
   let tableaus = deal(shuffledDeck);
-  const [pileHeart, updatePileHeart] = useState([]);
+  const [heartPile, heartPileUpdate] = useState([]);
+  const [spadePile, spadePileUpdate] = useState([]);
+  const [diamondPile, diamondPileUpdate] = useState([]);
+  const [clubPile, clubPileUpdate] = useState([]);
+
+  const buildingPiles = {
+    heart: {
+      pile: heartPile,
+      update: heartPileUpdate,
+    },
+    spade: {
+      pile: spadePile,
+      update: spadePileUpdate,
+    },
+    diamond: {
+      pile: diamondPile,
+      update: diamondPileUpdate,
+    },
+    club: {
+      pile: clubPile,
+      update: clubPileUpdate,
+    },
+  };
 
   const wasteReducer = (state, action) => {
     switch (action.type) {
-      case 'addCard': {
-        if(state.includes(action.card)) {
+      case "addCard": {
+        if (state.includes(action.card)) {
           return state;
         }
         return [...state, action.card];
       }
-      case 'moveWasteToDeck': {
+      case "removeTopCard": {
+        if (state.includes(action.card)) {
+          return state.pop();
+        }
+        return state;
+      }
+      case "moveWasteToDeck": {
         // this needs to move all cards back to the deck and preserve the order.
-        return []
+        return [];
       }
       default:
         throw new Error();
     }
   };
-  const [ waste, updateWaste ] = useReducer(wasteReducer, []);
+  const [waste, updateWaste] = useReducer(wasteReducer, []);
 
   const deckReducer = (state = shuffledDeck, action) => {
     switch (action.type) {
-      case 'addCardToWaste': {
+      case "addCardToWaste": {
         // this needs to pop a card off of the deck reducer and add it to the waste
         const newState = [...state];
         const flippedCard = newState.pop();
-        updateWaste({type: 'addCard', card: flippedCard});
+        updateWaste({ type: "addCard", card: flippedCard });
         return newState;
       }
-      case 'resetDeck': {
+      case "resetDeck": {
         // this needs to move all cards back to the shuffled deck
-        return []
+        return [];
       }
       default:
         throw new Error();
     }
   };
-  const [ deck, updateDeck ] = useReducer(deckReducer, shuffledDeck);
+  const [deck, updateDeck] = useReducer(deckReducer, shuffledDeck);
 
   const deckClickHandler = () => {
-    updateDeck({type: 'addCardToWaste'});
-  }
+    updateDeck({ type: "addCardToWaste" });
+  };
+  const wasteDoubleClickHandler = () => {
+    const topCard = waste[waste.length - 1];
+    console.log(`Top card in waste: ${topCard}`);
+    tryAddingCardToBuildingPile(topCard);
+  };
+
+  const tryAddingCardToBuildingPile = (card) => {
+    const [value, suit] = card.split(":");
+    console.log(`Try adding the ${value} of ${suit}s to their respective pile`);
+    if (buildingPiles[suit] && buildingPiles[suit].pile.length === 0 && value === 'ace') {
+      buildingPiles[suit].update([
+        ...buildingPiles[suit].pile,
+        card
+      ]);
+      updateWaste({
+        card,
+        type: 'removeTopCard'
+      });
+
+    } else if (buildingPiles[suit] && buildingPiles[suit].pile.length >= 1) {
+      // make sure cards are sequential
+    } else {
+      // throw an error
+      console.log(`BuildingPiles[suit] is defined: ${!!buildingPiles[suit]}`)
+      console.log(`buildingPiles[suit].pile.length: ${buildingPiles[suit].pile.length}`)
+      console.log(`card value: ${value}`)
+    }
+  };
 
   return (
     <>
       <section className="mt-20 bg-slate-800 size-full flex">
-        <div className="bg-slate-600 grow mx-2 p-4 justify-center">heart</div>
-        <div className="bg-slate-600 grow mx-2 p-4 justify-center">spade</div>
-        <div className="bg-slate-600 grow mx-2 p-4 justify-center">diamond</div>
-        <div className="bg-slate-600 grow mx-2 p-4 justify-center">club</div>
+        {/* Building piles */}
+        {(() => {
+          const elements = [];
+          for (let i = 0; i < suits.length; i++) {
+            const suit = suits[i];
+            elements.push(
+              <div
+                className="bg-slate-600 grow mx-2 p-4 justify-center"
+                key={`suits-${i}`}
+              >
+                <h2>{suit}</h2>
+                <Pile cards={buildingPiles[suit].pile} />
+              </div>
+            );
+          }
+
+          return elements;
+        })()}
+        {/* The waste */}
         <div className="bg-slate-600 grow mx-2 p-4 justify-center">
-        The waste ({waste.length} cards)
-        <Pile areFacedUp='true' cards={waste} />
+          <Pile areFacedUp="true" cards={waste} doubleClickHandler={wasteDoubleClickHandler} />
         </div>
+        {/* The deck */}
         <div className="bg-slate-600 grow mx-2 p-4 justify-center">
-          The deck ({deck.length} cards)
           <Pile cards={deck} clickHandler={deckClickHandler} />
         </div>
       </section>
